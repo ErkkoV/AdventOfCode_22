ad5b7a668ff82005d96befbd39ecfce758a55d1b
ErkkoV <erkko.vallivaara@gmail.com>
2022-12-09 07:25:14 +0200

    testing

--- test.cpp
+++ test.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+ 
 int main() {
-  cout << "Hello World!";
+  string toniPal = "Tonipal";
+  string kahville = "Kahville!";
+  
+  int myNum = 15;
+  
+  cout << toniPal;
+  cout << kahville;
+  
   return 0;
-}
+};
