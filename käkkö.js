420ae6aa5c1fb3f4c6c2ed9a88b778b574657c09
ErkkoV <erkko.vallivaara@gmail.com>
2022-12-14 07:20:23 +0200

    added vectors

---
+++ käkkö.js
@@ -0,0 +1,5 @@
+if (line == "") {
+    caloriesArray.push(0);
+} else {
+    caloriesarray[caloriesArray.length() - 1] += Number(line);
+}
---
+++ tonttuMissio7_1.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    string folder1;
+    string folder2;
+    string folder3;
+
+    ifstream readFolder1("folderinput.txt");
+    ifstream readFolder2("folderinput.txt");
+    ifstream readFolder3("folderinput.txt");
+
+    int firstFolder = 0;
+    string currentFolder = "root";
+    int folderNumber = 0;
+    vector<int> folderOrder;
+
+    vector<int> folderSpaces;
+    vector<string> folderNames;
+    vector<vector<int>> folderUnder;
+    vector<int> folderTotal;
+
+    folderSpaces.push_back(0);
+    folderNames.push_back("root");
+
+    while (getline (readFolder1, folder1)) {
+        // finds all folder names and assigns those into two vectors
+        if (folder1[0] == 'd') {
+            string folderName = folder1.substr(folder1.find(' ')+1);
+            bool noNewName = false;
+            for (int i = 0; i < folderNames.size(); i++) {
+                if (folderNames[i] == folderName) {
+                    noNewName = true;
+                    cout << "\n replace stuff";
+                } 
+            }
+            if (!noNewName) {
+                folderSpaces.push_back(0);
+                folderNames.push_back(folderName);
+                folderUnder.push_back({});
+                folderTotal.push_back(0);
+            }
+        }
+    }
+
+    while (getline (readFolder2, folder2)) {
+        // gets current folder name and updates folderspace size
+        if (folder2.substr(0, 5) == "$ cd " && folder2.substr(0, 7) != "$ cd ..") {
+            string folderName = folder2.substr(5);
+            currentFolder = folderName;
+        } else if (folder2[0] != '$' && folder2[0] != 'd') {
+            int filesize = stoi(folder2.substr(0, folder2.find(' ')));
+            for (int i = 0; i < folderNames.size(); i++) {
+                if (folderNames[i] == currentFolder) {
+                    folderSpaces[i] += filesize;
+                }
+            }
+        }
+    }
+
+    currentFolder = "root";
+
+    folderOrder.push_back(0);
+
+    while (getline (readFolder3, folder3)) {
+        // saves folders under each folder into vector
+        if (folder3[0] == '$') {
+            if (folder3.substr(0, 5) == "$ cd " && folder3.substr(0, 7) != "$ cd ..") {
+                string folderName = folder3.substr(5);
+                for (int i = 0; i < folderNames.size(); i++) {
+                    if (folderNames[i] == currentFolder) {
+                        bool contains = false;
+                        for (int fold : folderUnder[i]) {
+                            if (fold == i) {
+                                contains = true;
+                            }
+                        }
+
+                        if (!contains) {
+                            folderUnder[i].push_back(i);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < folderOrder.size(); i++) {
+                    cout << "\n" << folderOrder[i];
+                    for (int y = 0; y < folderUnder[folderOrder[i]].size(); y++) {
+                        cout << "," << folderUnder[folderOrder[i]][y];
+                    }
+                }
+
+
+                
+                
+                currentFolder = folderName;
+                for (int i = 0; i < folderNames.size(); i++) {
+                    if (folderNames[i] == currentFolder) {
+                        folderOrder.push_back(i);
+                    }
+                }
+            } else if (folder3 == "$ cd ..") {
+               folderOrder.pop_back();
+               currentFolder = folderNames[folderOrder[folderOrder.size() -1]];
+            } else if (folder3 == "$ cd /") {
+                folderOrder.clear();
+                folderOrder.push_back(0);
+                currentFolder = "root";
+            }
+        }
+    }
+
+    for (int i = 0; i < folderNames.size(); i++) {
+        folderTotal[i] = folderSpaces[i];
+        for (int y = 0; y < folderUnder[i].size(); y++) {
+            folderTotal[i] = folderTotal[i] + folderSpaces[folderUnder[i][y]];
+        }
+    }
+
+    
+    for (int i = 0; i < folderNames.size(); i++) {
+        cout << "\n" << folderNames[i];
+        cout << "\n" << folderSpaces[i];
+        // cout << "\n" << folderUnder[i];
+        cout << "\n" << folderTotal[i];
+    }
+
+     for (int i = 0; i < folderNames.size(); i++) {
+        if (folderTotal[i] < 100000) {
+            cout << "\n" << folderTotal[i];
+        }
+    }
+    
+    return 0;
+}
