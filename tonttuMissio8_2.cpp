01ac6433cd444363db848c18a223995629fc7570
ErkkoV <erkko.vallivaara@gmail.com>
2022-12-14 15:53:36 +0200

    working 8_2

--- tonttuMissio8_2.cpp
+++ tonttuMissio8_2.cpp
@@ -14,6 +14,8 @@ int main() {
     int lineHeight = 0;
     bool treeVisibility[99][99];
     int treeHeight[99][99];
+    int bestScore = 0;
+
     int inviTotal = 0;
 
 
@@ -29,60 +31,83 @@ int main() {
     
     for (int i = 0; i < 99; i++) {
         for (int y = 0; y < 99; y++) {
-            bool invisibleRight = false;
-            bool invisibleLeft = false;
-            bool invisibleUp = false;
-            bool invisibleDown = false;
-            
+
             int height = treeHeight[i][y];
             
-            for (int z = 0; z < 99; z++) {
-                if (z > y) {
-                    if (height <= treeHeight[i][z]) {
-                        invisibleRight = true;
-                    }
-                } else if (z < y) {
-                    if (height <= treeHeight[i][z]) {
-                        invisibleLeft = true;
-                    }
-                }
-                if (z > i) {
-                    if (height <= treeHeight[z][y]) {
-                        invisibleUp = true;
-                    }
-                } else if (z < i) {
-                    if (height <= treeHeight[z][y]) {
-                        invisibleDown = true;
-                    }
-                }
-            }
-
-            if (invisibleLeft && invisibleRight && invisibleUp && invisibleDown) {
-                treeVisibility[i][y] = false;
-                inviTotal += 1;
-            } else {
-                treeVisibility[i][y] = true;
-            }
+            int viewRight = 0;
+            int viewLeft = 0;
+            int viewDown = 0;
+            int viewUp = 0;
             
+            
+            for (int z = 1; z < 99; z++) {
+                if (y == 98) {
+                    break;
+                } else if (y + z == 98) {
+                    viewRight +=1;
+                    break;
+                } else if(height > treeHeight[i][y+z]) {
+                    viewRight +=1;
+                } else {
+                    viewRight +=1;
+                    break;
+                }
+            }
+            for (int z = 1; z < 99; z++) {
+                if (y == 0) {
+                    break;
+                } else if (y - z == 0) {
+                    viewLeft +=1;
+                    break;
+                } else if(height > treeHeight[i][y-z]) {
+                    viewLeft +=1;
+                } else {
+                    viewLeft +=1;
+                    break;
+                }
+            }
+            for (int z = 1; z < 99; z++) {
+                if (i == 98) {
+                    break;
+                } else if (i + z == 98) {
+                    viewDown +=1;
+                    break;
+                } else if(height > treeHeight[i+z][y]) {
+                    viewDown +=1;
+                } else {
+                    viewDown +=1;
+                    break;
+                }
+            }
+            for (int z = 1; z < 99; z++) {
+                if (y == 0) {
+                    break;
+                } else if (i - z == 0) {
+                    viewUp +=1;
+                    break;
+                } else if(height > treeHeight[i-z][y]) {
+                    viewUp +=1;
+                } else {
+                    viewUp +=1;
+                    break;
+                }
+            }
+
+            int best = viewUp * viewDown * viewLeft * viewRight;
+            if (best > bestScore) {
+                cout << "\n";
+                cout << "\n" << viewUp;
+                cout << "\n" << viewDown;
+                cout << "\n" << viewLeft;
+                cout << "\n" << viewRight;
+
+                bestScore = best;
+            }
         }
     }
 
 
-
-    for (int i = 0; i < 99; i++) {
-        cout << "\n";
-        for (int y = 0; y < 99; y++) {
-            cout << treeVisibility[i][y];
-        }
-    }
-    for (int i = 0; i < 99; i++) {
-        cout << "\n";
-        for (int y = 0; y < 99; y++) {
-            cout << treeHeight[i][y];
-        }
-    }
-
-    cout << "\n \n \n" << (99*99)-inviTotal;
+    cout << "\n \n \n" << bestScore;
 
    
     
