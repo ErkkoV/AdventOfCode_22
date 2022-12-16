d493317cf90e898444ee389508d9d2570b244555
ErkkoV <erkko.vallivaara@gmail.com>
2022-12-16 14:11:49 +0200

    tests

--- tonttuMissio11_1.cpp
+++ tonttuMissio11_1.cpp
@@ -9,10 +9,38 @@ using namespace std;
 
 class Monkey {
     public:
+        char oper;
+        int num;
+        int test;
+        int tmonk;
+        int fmonk;
         vector<int> items;
 
         void monkeyItem(int item) {
             items.push_back(item);
+        }
+
+        int monkeyOperation(int old) {
+            if (num == 0) {
+                return old * old;
+            } else if (oper == '*') {
+                return old * num;
+            } else {
+                return old + num;
+            }
+        }
+
+        int monkeyTest(int old) {
+            return monkeyOperation(old) % test; 
+        }
+
+        int monkeyReturn(int old) {
+            // true
+            if (!monkeyOperation(old)) {
+                return tmonk;
+            } else {
+                return fmonk;
+            }
         }
 };
 
@@ -36,11 +64,26 @@ int main() {
             currentMonkey = monkeys[7] -48;
         }
         if (monkeys[2] == 'S') {
-            vector<int> itemlist;
             for (int i = 18; i < monkeys.length(); i += 4) {
-                itemlist.push_back(stoi(monkeys.substr(i)));
+                monke[currentMonkey].monkeyItem(stoi(monkeys.substr(i)));
             }
-            monke[currentMonkey].items = itemlist;
+        }
+        if (monkeys[2] = 'O') {
+            monke[currentMonkey].oper = monkeys[24];
+            if (monkeys.substr(25) == "old") {
+                monke[currentMonkey].num = 0;
+            } else {
+                monke[currentMonkey].num = stoi(monkeys.substr(25));
+            }
+        }
+        if (monkeys[2] = 'T') {
+            monke[currentMonkey].test = stoi(monkeys.substr(25));
+        }
+        if (monkeys[7] = 't') {
+
+        }
+        if (monkeys[7] = 'f') {
+
         }
         
         
